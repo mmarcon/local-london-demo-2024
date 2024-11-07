20706efb30b1481a233da150903d12dd1f182487
Max Marcon <massimiliano.marcon@mongodb.com>
2024-11-07 12:08:43 +0100

    Indexes

--- bootstrap-demo-environment.sh
+++ bootstrap-demo-environment.sh
@@ -17,6 +17,19 @@ atlas deployments setup $DEPLOYMENT_NAME --type LOCAL --mdbVersion 8.0 --port $P
 mongorestore --port=$PORT --archive=demo/production-trips-w-reviews.archive
 atlas deployments search indexes create --file demo/search-index.json --deploymentName $DEPLOYMENT_NAME
 
+# Create a bunch of indexes so IntelliJ is happy
+indexes=(
+	"db.getSiblingDB('production').getCollection('trips').createIndex({ 'trip_status': 1, 'driver_id': 1 })"
+	"db.getSiblingDB('production').getCollection('trips').createIndex({ 'start_time': 1, 'end_time': 1 })"
+	"db.getSiblingDB('production').getCollection('trips').createIndex({ 'ratings.driver_rating': 1})"
+	"db.getSiblingDB('production').getCollection('trips').createIndex({ 'payment_method': 1})"
+	"db.getSiblingDB('production').getCollection('trips').createIndex({ 'passenger_id': 1})"
+	"db.getSiblingDB('production').getCollection('trips').createIndex({ 'dispute.assignee': 1})"
+)
+for i in "${indexes[@]}"; do
+	mongosh --port $PORT --eval "$i"
+done
+
 ## Restore empty TripRepository
 cp ride-share-api/res/TripsRepository.java ride-share-api/src/main/java/com/example/rideshareapi/repository/TripsRepository.java
 

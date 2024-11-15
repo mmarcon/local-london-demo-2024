68b93861cb55f0fbb72da49b94c91e7c3e87311f
Max Marcon <massimiliano.marcon@mongodb.com>
2024-11-15 18:20:17 +0100

    Right indexes

--- bootstrap-demo-environment.sh
+++ bootstrap-demo-environment.sh
@@ -18,8 +18,7 @@ mongorestore --port=$PORT --archive=demo/production-trips-w-reviews.archive
 atlas deployments search indexes create --file demo/search-index.json --deploymentName $DEPLOYMENT_NAME
 
 # Create a bunch of indexes so IntelliJ is happy
-indexes=(
-	"db.getSiblingDB('production').getCollection('trips').createIndex({ 'dispute.type': 1, 'total_fare': 1 })"	
+indexes=(	
 	"db.getSiblingDB('production').getCollection('trips').createIndex({ 'trip_status': 1, 'driver_id': 1 })"
 	"db.getSiblingDB('production').getCollection('trips').createIndex({ 'start_time': 1, 'end_time': 1 })"
 	"db.getSiblingDB('production').getCollection('trips').createIndex({ 'ratings.driver_rating': 1})"
